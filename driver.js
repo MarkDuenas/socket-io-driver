30f1d562b06bf89122e4c86f0e5dbd42dcbb6949
Mark Duenas <markgdlduenas@gmail.com>
2021-03-31 20:54:12 -0700

    complete

---
+++ driver.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const io = require("socket.io-client");
+
+const host = "http://localhost:3000";
+
+const socket = io.connect(host);
+
+socket.on("pickup", (payload) => {
+  setTimeout(() => {
+    console.log("*** PICKUP EVENT *** ");
+    console.log(new Date());
+    console.log(`DRIVER: picked up order ${payload.orderID}`);
+    console.log("================================");
+    socket.emit("in-transit", payload);
+  }, 1500);
+
+  setTimeout(() => {
+    console.log("*** DELIVERED EVENT ***");
+    socket.emit("delivered", payload);
+  }, 3000);
+});
